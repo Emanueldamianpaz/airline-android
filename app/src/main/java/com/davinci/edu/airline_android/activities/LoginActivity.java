632dae416b3f1f2c9ff16268e364a697b9200db1
Emanuel <emanueldamian.paz@gmail.com>
2017-11-11 19:42:48 -0300

    SharedPreferences implementado!

--- app/src/main/java/com/davinci/edu/airline_android/activities/LoginActivity.java
+++ app/src/main/java/com/davinci/edu/airline_android/activities/LoginActivity.java
@@ -1,11 +1,14 @@
 package com.davinci.edu.airline_android.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,29 +17,52 @@ import com.davinci.edu.airline_android.infraestructure.api.ApiClient;
 import com.google.gson.Gson;
 
 public class LoginActivity extends AppCompatActivity {
+    private Context context;
+
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        context = getBaseContext();
 
         final ApiClient apiClient = new ApiClient();
 
         final TextView usernameTxt = (TextView) findViewById(R.id.usernameTxt);
         final TextView passwordTxt = (TextView) findViewById(R.id.passwordTxt);
+        final CheckBox saveUserCheck = (CheckBox) findViewById(R.id.saveUserCheck);
         Button loginBtn = (Button) findViewById(R.id.loginBtn);
+
+        sharedPreferences = context.getSharedPreferences(getResources().getString(R.string.app_name), MODE_PRIVATE);
+
+        String username = sharedPreferences.getString("username", "");
+        String password = sharedPreferences.getString("password", "");
+
+        if (username != "" && password != "") {
+            usernameTxt.setText(username);
+            passwordTxt.setText(password);
+        }
+
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //if (apiClient.getUser(usernameTxt.getText().toString(), passwordTxt.getText().toString())) {
-                if (true) {
+                if (apiClient.getUser(usernameTxt.getText().toString(), passwordTxt.getText().toString())) {
 
-                    Intent intent = new Intent(getBaseContext(), ListItemActivity.class);
+                    if (saveUserCheck.isChecked()) {
+                        sharedPreferences = context.getSharedPreferences(getResources().getString(R.string.app_name), MODE_PRIVATE);
+                        sharedPreferences.edit()
+                                .putString("username", usernameTxt.getText().toString())
+                                .putString("password", passwordTxt.getText().toString())
+                                .apply();
+                    }
+
+                    Intent intent = new Intent(context, ListItemActivity.class);
                     intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    getBaseContext().startActivity(intent);
+                    context.startActivity(intent);
                 } else {
-                    Toast.makeText(getBaseContext(), "Login incorrecto!", Toast.LENGTH_LONG).show();
+                    Toast.makeText(context, "Login incorrecto!", Toast.LENGTH_LONG).show();
                 }
 
             }
