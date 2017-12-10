669d87f3e340f13b98fc6ddcfae6aaa0144d2556
Emanuel <emanueldamian.paz@gmail.com>
2017-12-09 22:03:52 -0300

    Se agrego navbar y menu, agregar funcionalidad correcta al carrito

--- app/src/main/java/com/davinci/edu/airline_android/activities/ListItemActivity.java
+++ app/src/main/java/com/davinci/edu/airline_android/activities/ListItemActivity.java
@@ -1,9 +1,19 @@
 package com.davinci.edu.airline_android.activities;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.davinci.edu.airline_android.R;
 import com.davinci.edu.airline_android.infraestructure.FlightAdapter;
@@ -14,7 +24,7 @@ import com.davinci.edu.airline_android.infraestructure.models.Flight;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ListItemActivity extends AppCompatActivity {
+public class ListItemActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +32,18 @@ public class ListItemActivity extends AppCompatActivity {
         setContentView(R.layout.activity_list_item);
 
         final ApiClient apiClient = new ApiClient(getBaseContext());
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
 
         final ProgressDialog progressDialog = ProgressDialog.show(this, "Vuelos", "Obteniendo el menu...", true, false);
 
@@ -36,6 +58,40 @@ public class ListItemActivity extends AppCompatActivity {
             }
         });
 
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.contact:
+                Intent sendEmail = new Intent(Intent.ACTION_SENDTO);
+                sendEmail.setType("text/plain");
+                Intent.createChooser(sendEmail, "Encviar email");
+                break;
+            case R.id.close_session:
+                Intent login = new Intent(this, LoginActivity.class);
+                startActivity(login);
+                finish();
+                break;
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
 
     }
 }
